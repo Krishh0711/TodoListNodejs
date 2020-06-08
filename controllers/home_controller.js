2531ffa45aa4b989686a350b821016726d1f6b0b
Krishh258 <krishnanshu33@gmail.com>
2020-06-08 19:44:30 +0530

    added display of todo to  home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,15 @@
-module.exports.home = function(req, res){
-    // return res.end('Express is up');
-    return res.render('home',{
-        title: "Home1"
+const mongoose = require('mongoose');
+const TodoList = require('../models/list');
+
+module.exports.home = function(request, response){
+    TodoList.find({},function(err,todolist){
+        if(err){
+            console.log('error in fetching list from db');
+        }
+        return response.render('home',{
+            title:'TodoList',
+            todolist:todolist
+        });
     });
+    
 }
